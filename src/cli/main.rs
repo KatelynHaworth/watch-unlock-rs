ec73174458ea6ead7cd6350cf2714a54beed5f24
Katelyn 'KatLongLegs' Haworth <katelyn@haworth.id.au>
2026-02-16 04:52:58 +1100

    Refactored the binary to be a CLI using clap, sets the ground work for further features

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -4,30 +4,44 @@ mod watch;
 use crate::watch::AppleWatch;
 
 use base64::{engine::general_purpose::STANDARD, Engine as _};
+use clap::{Arg, ArgMatches, Command};
 use std::env;
 use std::process::exit;
 use std::time::Duration;
 
+fn cli_commands() -> [Command; 1] {
+    [Command::new("query_status")
+        .about("Queries the current status of an Apple Watch")
+        .arg(
+            Arg::new("irk")
+                .required(true)
+                .help("Identity Resolution Key, in base64, of the Apple Watch to query"),
+        )]
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Invalid usage");
-        eprintln!();
+    let mut cmd = Command::new(env!("CARGO_CRATE_NAME")).subcommands(cli_commands());
+    let matches = cmd.get_matches_mut();
 
-        eprintln!("Usage:");
-        eprintln!("\t{} [identity_resolution_key]", args.first().unwrap());
+    #[allow(clippy::single_match_else)]
+    let status_code = match matches.subcommand() {
+        Some(("query_status", args)) => query_status(args).await,
+        _ => {
+            let _ = cmd.print_help();
+            1
+        }
+    };
 
-        eprintln!("Parameters:");
-        eprintln!(
-            "\tidentity_resolution_key - The base64 IRK required to match the desired Apple Watch"
-        );
-        exit(1);
-    }
+    exit(status_code);
+}
+
+async fn query_status(args: &ArgMatches) -> i32 {
+    let encoded_irl: &String = args.get_one("irk").expect("required argument");
 
     println!("Decoding Identity Resolution Key for Apple Watch");
     let mut raw_irk: [u8; 16] = [0; 16];
-    match STANDARD.decode_slice(args.get(1).unwrap(), &mut raw_irk[..]) {
+    match STANDARD.decode_slice(encoded_irl, &mut raw_irk[..]) {
         Err(err) => {
             eprintln!("Failed to decode IRK: {err}");
             exit(1)
@@ -41,13 +55,23 @@ async fn main() {
 
     let mut watch = AppleWatch::new(raw_irk);
 
-    println!("Connecting to BT system");
+    println!("Creating Bluetooth session");
+    let session = match bluer::Session::new().await {
+        Ok(session) => session,
+        Err(err) => {
+            println!("Failed to create Bluetooth session: {err}");
+            return 1;
+        }
+    };
 
-    let session = bluer::Session::new().await.expect("open BT session");
-    let adapter = session
-        .default_adapter()
-        .await
-        .expect("obtain default adapter");
+    println!("Selecting default Bluetooth adapter");
+    let adapter = match session.default_adapter().await {
+        Ok(adapter) => adapter,
+        Err(err) => {
+            println!("Failed to obtain access to default Bluetooth adapter: {err}");
+            return 1;
+        }
+    };
 
     println!("Searching for Apple Watch");
     match watch
@@ -56,16 +80,30 @@ async fn main() {
     {
         Err(err) => {
             println!("Failed to find Apple Watch: {err}");
-            exit(1)
+            return 1;
         }
         Ok(tries) => println!("Found Apple Watch after {tries} tries"),
     }
 
-    match watch.get_watch_status().await {
+    let status = match watch.get_watch_status().await {
+        Ok(status) => status,
         Err(err) => {
             println!("Failed to get Apple Watch status: {err}");
-            exit(1)
+            return 1;
         }
-        Ok(status) => println!("Got Apple Watch status: {status:?}"),
-    }
+    };
+
+    println!("Apple Watch Status");
+    println!(
+        "\tAddress.......................: {}",
+        watch.get_watch_address()
+    );
+    println!("\tRSSI..........................: {}", status.rssi);
+    println!("\tUnlocked......................: {}", status.locked);
+    println!(
+        "\tAuto-unlock devices enabled...: {}",
+        status.device_auto_unlock_enabled
+    );
+
+    0
 }
